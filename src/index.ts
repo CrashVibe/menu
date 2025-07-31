7c9f0fbe5827f21c47c0ec3c7eb0ba441402192a
MrlingXD <wlingzhenyu@163.com>
2025-07-31 17:13:25 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Context, Schema } from "koishi";
+
+export const name = "menu";
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({});
+
+export async function apply(ctx: Context) {
+    ctx.command("菜单")
+        .alias("menu")
+        .action(async () => {
+            return "使用文档：\nhttps://u1.crashvibe.cn/intro";
+        });
+}
